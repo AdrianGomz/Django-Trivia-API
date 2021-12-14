8e27981b3c1ff67e9c5df50d00fbc0ec82bf5345
isaac <isaac>
2021-12-13 23:37:57 -0600

    created custom premissions so that only authors can modify or delete their questions

--- Questions/urls.py
+++ Questions/urls.py
@@ -3,18 +3,6 @@ from .views import QuestionsViewset
 from rest_framework.routers import DefaultRouter
 
 
-question_list = QuestionsViewset.as_view({
-
-    'get': 'list',
-    'post': 'create'
-})
-question_detail = QuestionsViewset.as_view({
-    'get': 'retrive',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
 router = DefaultRouter()
 router.register('question', QuestionsViewset)
 urlpatterns = [
