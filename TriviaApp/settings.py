38d8fc8ac2ac0e801d5f497876e25584f3b538b7
isaac <vi13150@innovaccion.mx>
2022-07-13 21:57:04 -0600

    changes for prod

--- TriviaApp/settings.py
+++ TriviaApp/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -90,10 +92,9 @@ WSGI_APPLICATION = 'TriviaApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 REST_FRAMEWORK = {
